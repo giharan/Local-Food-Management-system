afd33242871f60ecebe69216344ee6d251730fea
Gihara Nishadi <nishadigihara@gmail.com>
2026-09-01 21:31:21 +0800

    Add files via upload
    Add frontend project files - assets

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,398 @@
+import basket_icon from './basket_icon.png'
+import logo from './logo.png'
+import header_img from './header_img.png'
+import search_icon from './search_icon.png'
+import menu_1 from './menu_1.png'
+import menu_2 from './menu_2.png'
+import menu_3 from './menu_3.png'
+import menu_4 from './menu_4.png'
+import menu_5 from './menu_5.png'
+import menu_6 from './menu_6.png'
+import menu_7 from './menu_7.png'
+import menu_8 from './menu_8.png'
+import menu_9 from './menu_9.png'
+
+import food_1 from './food_1.png'
+import food_2 from './food_2.png'
+import food_3 from './food_3.png'
+import food_4 from './food_4.png'
+import food_5 from './food_5.png'
+import food_6 from './food_6.png'
+import food_7 from './food_7.png'
+import food_8 from './food_8.png'
+import food_9 from './food_9.png'
+import food_10 from './food_10.png'
+import food_11 from './food_11.png'
+import food_12 from './food_12.png'
+import food_13 from './food_13.png'
+import food_14 from './food_14.png'
+import food_15 from './food_15.png'
+import food_16 from './food_16.png'
+import food_17 from './food_17.png'
+import food_18 from './food_18.png'
+import food_19 from './food_19.png'
+import food_20 from './food_20.png'
+import food_21 from './food_21.png'
+import food_22 from './food_22.png'
+import food_23 from './food_23.png'
+import food_24 from './food_24.png'
+import food_25 from './food_25.png'
+import food_26 from './food_26.png'
+import food_27 from './food_27.png'
+import food_28 from './food_28.png'
+import food_29 from './food_29.png'
+import food_30 from './food_30.png'
+import food_31 from './food_31.png'
+import food_32 from './food_32.png'
+
+import add_icon_white from './add_icon_white.png'
+import add_icon_green from './add_icon_green.png'
+import remove_icon_red from './remove_icon_red.png'
+import app_store from './app_store.png'
+import play_store from './play_store.png'
+import linkedin_icon from './linkedin_icon.png'
+import facebook_icon from './facebook_icon.png'
+import twitter_icon from './twitter_icon.png'
+import cross_icon from './cross_icon.png'
+import selector_icon from './selector_icon.png'
+import rating_starts from './rating_starts.png'
+import profile_icon from './profile_icon.png'
+import bag_icon from './bag_icon.png'
+import logout_icon from './logout_icon.png'
+import parcel_icon from './parcel_icon.png'
+import copy_icon from './copylink.png'
+import bannergirl_image from './bannergirl.png'
+import voucher from './voucher.png'
+
+import jayanga from './J.jpg'
+import vimukthi from './V.jpg'
+import gihara from './G.jpg'
+import hashan from './H.jpg'
+import irushi from './I.jpg'
+import sithija from './Sithija.jpg'
+import sashi from './S.jpg'
+
+
+export const assets = {
+    logo,
+    basket_icon,
+    header_img,
+    search_icon,
+    rating_starts,
+    add_icon_green,
+    add_icon_white,
+    remove_icon_red,
+    app_store,
+    play_store,
+    linkedin_icon,
+    facebook_icon,
+    twitter_icon,
+    cross_icon,
+    selector_icon,
+    profile_icon,
+    logout_icon,
+    bag_icon,
+    parcel_icon,
+    copy_icon,
+    bannergirl_image,
+    voucher,
+    jayanga,
+    sithija,
+    sashi,
+    hashan,
+    vimukthi,
+    gihara,
+    irushi
+}
+
+export const menu_list = [
+    {
+       menu_name: "Central Province",
+        menu_image: menu_1
+    },
+    {
+        menu_name: "Eastern Province",
+        menu_image: menu_2
+    },
+    {
+        menu_name: "North Central Province",
+        menu_image: menu_3
+    },
+    {
+        menu_name: "North Western Province",
+        menu_image: menu_4
+    },
+    {
+        menu_name: "Northern Province",
+        menu_image: menu_5
+    },
+    {
+        menu_name: "Sabaragamuwa Province",
+        menu_image: menu_6
+    },
+    {
+       menu_name: "Southern Province",
+        menu_image: menu_7
+    },
+    {
+        menu_name: "Uva Province",
+        menu_image: menu_8
+    },
+    {
+        menu_name: "Western Province",
+        menu_image: menu_9
+    }]
+
+export const food_list = [
+    {
+        _id: "1",
+        name: "Aluwa",
+        image: food_1,
+        price: 80,
+        unit: "Include 10 Pices",
+        description: "A traditional Sri Lankan sweet made from rice flour, coconut, and treacle, often flavored with spices.",
+        category: "Central Province"
+    },
+    {
+        _id: "2",
+        name: "Kalu Dhodhol",
+        image: food_2,
+        price: 1200,
+        unit: "Include 1Kg",
+        description: " A rich, dark sweet made with coconut milk, jaggery, and rice flour, cooked slowly to perfection.",
+        category: "Central Province"
+    }, {
+        _id: "3",
+        name: "Kiri Toffee",
+        image: food_3,
+        price: 50,
+        unit: "Include 10 Pices",
+        description: "A milk-based fudge-like sweet, made with condensed milk and sugar.",
+        category: "Central Province"
+    }, {
+        _id: "4",
+        name: "Munkawum",
+        image: food_4,
+        price: 100,
+        unit: "Include 10 Pices",
+        description: "A deep-fried sweet snack made from rice flour, coconut, and treacle, shaped like small balls.",
+        category: "Central Province"
+    }, {
+        _id: "5",
+        name: "Cuttlefish Curry",
+        image: food_5,
+        price: 450,
+        description: "A flavorful and spicy curry made with tender cuttlefish, cooked in a blend of Sri Lankan spices.",
+        category: "Eastern Province"
+    }, {
+        _id: "6",
+        name: "Palmyra Jaggery",
+        image: food_6,
+        price: 100,
+        unit: "Include 10 Pices",
+        description: "A natural sweetener made from the sap of the Palmyra palm.",
+        category: "Eastern Province"
+    }, {
+        _id: "7",
+        name: "Pineapple Kesari",
+        image: food_7,
+        price: 320,
+        description: "A sweet semolina-based dessert infused with pineapple flavor, colored a vibrant yellow.",
+        category: "Eastern Province"
+    }, {
+        _id: "8",
+        name: "Therali",
+        image: food_8,
+        price: 25,
+        unit: "Include 1 Pices",
+        description: "A traditional sweet snack made from steamed rice flour and coconut, wrapped in leaves.",
+        category: "Eastern Province"
+    }, {
+        _id: "9",
+        name: "Halapa",
+        image: food_9,
+        price: 30,
+        unit: "Include 1 Pices",
+        description: "A steamed, leaf-wrapped snack made from kurakan flour, coconut and with a distinct flavor.",
+        category: "North Central Province"
+    }, {
+        _id: "10",
+        name: "Fresh Water Fish Curry",
+        image: food_10,
+        price: 550,
+        description: "A tangy and spicy curry made with freshwater fish, flavored with tamarind and aromatic spices.",
+        category: "North Central Province"
+    }, {
+        _id: "11",
+        name: "Kurakkan Roti",
+        image: food_11,
+        price: 20,
+        unit: "Include 1 Pices",
+        description: "A flatbread made from finger millet kurakkan flour, often served with spicy coconut sambol.",
+        category: "North Central Province"
+    }, {
+        _id: "12",
+        name: "Wali Thalapa",
+        image: food_12,
+        price: 80,
+        unit: "Include 10 Pices",
+        description: "A sweet, sticky treat made from rice flour and treacle, often enriched with nuts and spices.",
+        category: "North Central Province"
+    },
+    {
+        _id: "13",
+        name: "Bibikkan",
+        image: food_13,
+        price: 20,
+        unit: "Include 1 Pices",
+        description: "A traditional coconut cake made with grated coconut, jaggery, and spices.",
+        category: "North Western Province"
+    },
+    {
+        _id: "14",
+        name: "Crab Curry",
+        image: food_14,
+        price: 1000,
+        description: "A deliciously spicy curry made with fresh crab, simmered in a blend of coconut milk and spices.",
+        category: "North Western Province"
+    }, {
+        _id: "15",
+        name: "Prawn Devel",
+        image: food_15,
+        price: 900,
+        description: "A spicy and tangy prawn dish stir-fried with onions, tomatoes, and Sri Lankan spices.",
+        category: "North Western Province"
+    }, {
+        _id: "16",
+        name: "Rasam",
+        image: food_16,
+        price: 350,
+        description: "A hot, sour, and spicy soup made with tamarind, tomatoes, and a variety of spices.",
+        category: "North Western Province"
+    }, {
+        _id: "17",
+        name: "Jaffna Thosee",
+        image: food_17,
+        price: 30,
+        unit: "Include 1 Pices",
+        description: "A crispy and thin pancake made from fermented rice and lentil batter, popular in the Jaffna region.",
+        category: "Nothern Province"
+    }, {
+        _id: "18",
+        name: "Mango Kesari",
+        image: food_18,
+        price: 250,
+        description: "A semolina-based dessert flavored with ripe mango puree, garnished with nuts and raisins.",
+        category: "Nothern Province"
+    }, {
+        _id: "19",
+        name: "Musket",
+        image: food_19,
+        price: 150,
+        unit: "Include 10 Pices",
+        description: "A dense and rich sweet cake made with coconut and semolina, flavored with cardamom.",
+        category: "Nothern Province"
+    }, {
+        _id: "20",
+        name: "Uludu Wadee",
+        image: food_20,
+        price: 40,
+        unit: "Include 1 Pices",
+        description: "A savory fritter made from black gram (ulundu) lentils, spiced, onions, green chilies, curry leaves.",
+        category: "Nothern Province"
+    }, {
+        _id: "21",
+        name: "Thala Guli",
+        image: food_21,
+        price: 150,
+        unit: "Include 10 Pices",
+        description: "A traditional sweet made from sesame seeds and jaggery, rolled into bite-sized balls.",
+        category: "Sabaragamuwa Province"
+    }, {
+        _id: "22",
+        name: "Aggalaa",
+        image: food_22,
+        price: 100,
+        unit: "Include 10 Pices",
+        description: "A sweet snack made from roasted rice and treacle, shaped into small balls.",
+        category: "Sabaragamuwa Province"
+    }, {
+        _id: "23",
+        name: "Thala Karali",
+        image: food_23,
+        price: 100,
+        unit: "Include 10 Pices",
+        description: "A crispy sweet made from sesame seeds and sugar syrup, often cut into diamond shapes.",
+        category: "Sabaragamuwa Province"
+    }, {
+        _id: "24",
+        name: "Wambotu Moju",
+        image: food_24,
+        price: 220,
+        description: "A tangy and spicy pickle made from brinjals eggplants, vinegar, and spices.",
+        category: "Sabaragamuwa Province"
+    },
+    {
+        _id: "25",
+        name: "Fish Abul Thiyal",
+        image: food_25,
+        price: 430,
+        description: "A sour and spicy fish dish cooked with tamarind and black pepper, known for its long shelf life.",
+        category: "Southern Province"
+    },
+    {
+        _id: "26",
+        name: "Prawn Wadee",
+        image: food_26,
+        price: 50,
+        unit: "Include 1 Pices",
+        description: "A crispy fritter made from a batter of lentils and prawns, seasoned with spices and deep-fried.",
+        category: "Southern Province"
+    }, {
+        _id: "27",
+        name: "Konda Kawum",
+        image: food_27,
+        price: 150,
+        unit: "Include 10 Pices",
+        description: "A deep-fried sweet made from rice flour and treacle, with a soft inside and a crispy outside.",
+        category: "Southern Province"
+    }, {
+        _id: "28",
+        name: "Mee Kiri",
+        image: food_28,
+        price: 350,
+        description: "Fresh buffalo curd, often served with treacle as a dessert or enjoyed as a side dish.",
+        category: "Southern Province"
+    }, {
+        _id: "29",
+        name: "Kithul Hakuru",
+        image: food_29,
+        price: 550,
+        unit: "Include 1 Pices",
+        description: "A type of jaggery made from the sap of the Kithul palm, used as a natural sweetener.",
+        category: "Uva Province"
+    }, {
+        _id: "30",
+        name: "Kithul Pani",
+        image: food_30,
+        price: 1500,
+        unit: "Include 1 liter",
+        description: "The sweet syrup made from the sap of the Kithul palm.",
+        category: "Uva Province"
+    }, {
+        _id: "31",
+        name: "Kottu",
+        image: food_31,
+        price: 750,
+        description: "A popular street food made by stir-frying chopped roti with vegetables, eggs, or meat, and spices.",
+        category: "Western Province"
+    }, {
+        _id: "32",
+        name: "Pane Cacke",
+        image: food_32,
+        price: 40,
+        unit: "Include 1 Pices",
+        description: "A soft and fluffy Sri Lankan-style pancake, often filled with sweet coconut or jaggery mixture.",
+        category: "Western Province"
+    }
+]
